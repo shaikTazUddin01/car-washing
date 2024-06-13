cb4fc68961161d2a2d61416fbd38aa1fd8c889b6
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-06-13 11:15:01 +0600

    set up a new project

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express'
+
+
+const app = express()
+
+
+app.get('/', (req:Request, res :Response) => {
+  res.send('server is connecting')
+})
+
+
+export default app;
